f5f84e9bfd5781ba7c680527abc7a9feb557dbe1
ritaaktay <ritaaktay@gmail.com>
2022-11-09 12:37:09 +0000

    merged latest main

--- space_invaders/main.js
+++ space_invaders/main.js
@@ -8,21 +8,21 @@ const GAME_WIDTH = 720;
 const GAME_HEIGHT = 360;
 
 const STATE = {
-  x_pos : 0,
-  y_pos : 0,
+  x_pos: 0,
+  y_pos: 0,
   move_right: false,
   move_left: false,
   shoot: false,
   lasers: [],
   enemyLasers: [],
-  enemies : [],
+  enemies: [],
   spaceship_width: 50,
   enemy_width: 50,
-  cooldown : 0,
+  cooldown: 0,
   number_of_enemies: 14,
-  enemy_cooldown : 0,
-  gameOver: false
-}
+  enemy_cooldown: 0,
+  gameOver: false,
+};
 
 // General purpose functions
 function setPosition($element, x, y) {
@@ -34,51 +34,54 @@ function setSize($element, width) {
   $element.style.height = "auto";
 }
 
-function bound(x){
-  if (x >= GAME_WIDTH-STATE.spaceship_width){
-    STATE.x_pos = GAME_WIDTH-STATE.spaceship_width;
-    return GAME_WIDTH-STATE.spaceship_width
-  } if (x <= 0){
+function bound(x) {
+  if (x >= GAME_WIDTH - STATE.spaceship_width) {
+    STATE.x_pos = GAME_WIDTH - STATE.spaceship_width;
+    return GAME_WIDTH - STATE.spaceship_width;
+  }
+  if (x <= 0) {
     STATE.x_pos = 0;
-    return 0
+    return 0;
   } else {
     return x;
   }
 }
 
-function collideRect(rect1, rect2){
-  return!(rect2.left > rect1.right || 
-    rect2.right < rect1.left || 
-    rect2.top > rect1.bottom || 
-    rect2.bottom < rect1.top);
+function collideRect(rect1, rect2) {
+  return !(
+    rect2.left > rect1.right ||
+    rect2.right < rect1.left ||
+    rect2.top > rect1.bottom ||
+    rect2.bottom < rect1.top
+  );
 }
 
-// Enemy 
-function createEnemy($container, x, y){
+// Enemy
+function createEnemy($container, x, y) {
   const $enemy = document.createElement("img");
   $enemy.src = "img/ufo.png";
   $enemy.className = "enemy";
   $container.appendChild($enemy);
-  const enemy_cooldown = Math.floor(Math.random()*100);
-  const enemy = {x, y, $enemy, enemy_cooldown}
+  const enemy_cooldown = Math.floor(Math.random() * 100);
+  const enemy = { x, y, $enemy, enemy_cooldown };
   STATE.enemies.push(enemy);
   setSize($enemy, STATE.enemy_width);
-  setPosition($enemy, x, y)
+  setPosition($enemy, x, y);
 }
 
-function updateEnemies($container){
-  const dx = Math.sin(Date.now()/1000)*40;
-  const dy = Math.cos(Date.now()/1000)*30;
+function updateEnemies($container) {
+  const dx = Math.sin(Date.now() / 1000) * 40;
+  const dy = Math.cos(Date.now() / 1000) * 30;
   const enemies = STATE.enemies;
-  for (let i = 0; i < enemies.length; i++){
+  for (let i = 0; i < enemies.length; i++) {
     const enemy = enemies[i];
     var a = enemy.x + dx;
     var b = enemy.y + dy;
     setPosition(enemy.$enemy, a, b);
-    enemy.cooldown = Math.random(0,100);
-    if (enemy.enemy_cooldown == 0){
+    enemy.cooldown = Math.random(0, 100);
+    if (enemy.enemy_cooldown == 0) {
       createEnemyLaser($container, a, b);
-      enemy.enemy_cooldown = Math.floor(Math.random()*50)+100 ;
+      enemy.enemy_cooldown = Math.floor(Math.random() * 50) + 100;
     }
     enemy.enemy_cooldown -= 0.5;
   }
@@ -96,51 +99,57 @@ function createPlayer($container) {
   setSize($player, STATE.spaceship_width);
 }
 
-function updatePlayer(){
-  if(STATE.move_left){
+function updatePlayer() {
+  if (STATE.move_left) {
     STATE.x_pos -= 3;
-  } if(STATE.move_right){
+  }
+  if (STATE.move_right) {
     STATE.x_pos += 3;
-  } if(STATE.shoot && STATE.cooldown == 0){
-    createLaser($container, STATE.x_pos - STATE.spaceship_width/2, STATE.y_pos);
+  }
+  if (STATE.shoot && STATE.cooldown == 0) {
+    createLaser(
+      $container,
+      STATE.x_pos - STATE.spaceship_width / 2,
+      STATE.y_pos
+    );
     STATE.cooldown = 30;
   }
   const $player = document.querySelector(".player");
-  setPosition($player, bound(STATE.x_pos), STATE.y_pos-10);
-  if(STATE.cooldown > 0){
+  setPosition($player, bound(STATE.x_pos), STATE.y_pos - 10);
+  if (STATE.cooldown > 0) {
     STATE.cooldown -= 0.5;
   }
 }
 
 // Player Laser
-function createLaser($container, x, y){
+function createLaser($container, x, y) {
   const $laser = document.createElement("img");
   $laser.src = "img/laser.png";
   $laser.className = "laser";
   $container.appendChild($laser);
-  const laser = {x, y, $laser};
+  const laser = { x, y, $laser };
   STATE.lasers.push(laser);
   setPosition($laser, x, y);
 }
 
-function updateLaser($container){
+function updateLaser($container) {
   const lasers = STATE.lasers;
-  for(let i = 0; i < lasers.length; i++){
+  for (let i = 0; i < lasers.length; i++) {
     const laser = lasers[i];
     laser.y -= 2;
-    if (laser.y < 0){
+    if (laser.y < 0) {
       deleteLaser(lasers, laser, laser.$laser);
     }
     setPosition(laser.$laser, laser.x, laser.y);
     const laser_rectangle = laser.$laser.getBoundingClientRect();
     const enemies = STATE.enemies;
-    for(let j = 0; j < enemies.length; j++){
+    for (let j = 0; j < enemies.length; j++) {
       const enemy = enemies[j];
       const enemy_rectangle = enemy.$enemy.getBoundingClientRect();
-      if(collideRect(enemy_rectangle, laser_rectangle)){
+      if (collideRect(enemy_rectangle, laser_rectangle)) {
         deleteLaser(lasers, laser, laser.$laser);
         const index = enemies.indexOf(enemy);
-        enemies.splice(index,1);
+        enemies.splice(index, 1);
         $container.removeChild(enemy.$enemy);
       }
     }
@@ -148,37 +157,43 @@ function updateLaser($container){
 }
 
 // Enemy Laser
-function createEnemyLaser($container, x, y){
+function createEnemyLaser($container, x, y) {
   const $enemyLaser = document.createElement("img");
   $enemyLaser.src = "img/enemyLaser.png";
   $enemyLaser.className = "enemyLaser";
   $container.appendChild($enemyLaser);
-  const enemyLaser = {x, y, $enemyLaser};
+  const enemyLaser = { x, y, $enemyLaser };
   STATE.enemyLasers.push(enemyLaser);
   setPosition($enemyLaser, x, y);
 }
 
-function updateEnemyLaser($container){
+function updateEnemyLaser($container) {
   const enemyLasers = STATE.enemyLasers;
-  for(let i = 0; i < enemyLasers.length; i++){
+  for (let i = 0; i < enemyLasers.length; i++) {
     const enemyLaser = enemyLasers[i];
     enemyLaser.y += 2;
-    if (enemyLaser.y > GAME_HEIGHT-30){
+    if (enemyLaser.y > GAME_HEIGHT - 30) {
       deleteLaser(enemyLasers, enemyLaser, enemyLaser.$enemyLaser);
     }
     const enemyLaser_rectangle = enemyLaser.$enemyLaser.getBoundingClientRect();
-    const spaceship_rectangle = document.querySelector(".player").getBoundingClientRect();
-    if(collideRect(spaceship_rectangle, enemyLaser_rectangle)){
+    const spaceship_rectangle = document
+      .querySelector(".player")
+      .getBoundingClientRect();
+    if (collideRect(spaceship_rectangle, enemyLaser_rectangle)) {
       STATE.gameOver = true;
     }
-    setPosition(enemyLaser.$enemyLaser, enemyLaser.x + STATE.enemy_width/2, enemyLaser.y+15);
+    setPosition(
+      enemyLaser.$enemyLaser,
+      enemyLaser.x + STATE.enemy_width / 2,
+      enemyLaser.y + 15
+    );
   }
 }
 
 // Delete Laser
-function deleteLaser(lasers, laser, $laser){
+function deleteLaser(lasers, laser, $laser) {
   const index = lasers.indexOf(laser);
-  lasers.splice(index,1);
+  lasers.splice(index, 1);
   $container.removeChild($laser);
 }
 
@@ -204,24 +219,25 @@ function KeyRelease(event) {
 }
 
 // Main Update Function
-function update(){
+function update() {
   updatePlayer();
   updateEnemies($container);
   updateLaser($container);
   updateEnemyLaser($container);
 
   window.requestAnimationFrame(update);
-  
+
   if (STATE.gameOver) {
     document.querySelector(".lose").style.display = "block";
-  } if (STATE.enemies.length == 0) {
+  }
+  if (STATE.enemies.length == 0) {
     document.querySelector(".win").style.display = "block";
   }
 }
 
 function createEnemies($container) {
-  for(var i = 0; i <= STATE.number_of_enemies/2; i++){
-    createEnemy($container, i*80, 60);
+  for (var i = 0; i <= STATE.number_of_enemies / 2; i++) {
+    createEnemy($container, i * 80, 60);
   }
   // } for(var i = 0; i <= STATE.number_of_enemies/2; i++){
   //   createEnemy($container, i*80, 180);
